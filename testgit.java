ab473648cc4621c418bd5ac79d9c68f09f666122
RedaMacbook <Reda.f95@hotmail.com>
2025-02-07 12:52:54 +0100

    Remove deleted files

--- testgit.java
+++
@@ -1,9 +0,0 @@
-class Test {
-	public static void main (String[]args) {
-	System.out.print("Hei git" );
-<<<<<<< HEAD
-=======
-	System.out.print("Nye ordet som er lagt til" );
->>>>>>> ny_funksjon
-	}
-}
